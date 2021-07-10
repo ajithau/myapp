f7cff51af2464e5131601465c7afda920ac1ccb9
ajith <ajith.au@fingent.com>
2021-07-10 14:07:54 +0530

    gallery model

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 <main>
-
-    <form action="{{ route('products.store') }}" method="POST"> 
+    @if ($errors->any())
+        <div class="alert alert-danger text-red-500">
+            <ul>
+                @foreach ($errors->all() as $message)
+                <li>{{ $message }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data"> 
             {{ csrf_field() }}
         
             <div class="row">
@@ -38,7 +46,13 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong>Image:</strong>
-                        <input type="text" name="image" class="form-control" placeholder="Image">
+                        <input type="file" name="image" class="form-control">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Gallery images:</strong>
+                        <input type="file" name="gallery[]" class="form-control" multiple>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12 text-center">
